71cd857eeb37813f37af397ea6b315ca6ab1f7d9
Naveen <adith.naveen@gmail.com>
2022-06-21 15:32:08 +0530

    updated with addContact code

--- web-works/express-js-works/contact-application/app1.js
+++ web-works/express-js-works/contact-application/app1.js
@@ -5,12 +5,30 @@ const ContactService = require('./services/ContactServiceAsync');
 const cs = new ContactService();
 
 console.log("*** Start ***");
-cs.getContactById(1, (err, data) => {
+// cs.getContactById(1, (err, data) => {
+//     if (err) {
+//         console.log("Error ", err);
+//     } else {
+//         console.log(data);
+//     }
+
+// })
+
+
+
+let newContact = { 
+    name: "Kumkum Ram",
+    email: "kumkum@ps.com", 
+    phone: 7788996655, 
+    city: "Mysuru" 
+};
+
+cs.addContact(newContact, (err, data) => {
     if (err) {
-        console.log("Error ", err);
+        console.log("Error : ", err);
     } else {
         console.log(data);
     }
-
 })
+
 console.log("*** End ***");
--- web-works/express-js-works/contact-application/services/ContactServiceAsync.js
+++ web-works/express-js-works/contact-application/services/ContactServiceAsync.js
@@ -40,8 +40,55 @@ class ContactService {
             callback(null, contact);
 
         }, 0);
+    }
+
+    addContact(contact, callback) {
+        if (!callback || typeof callback !== 'function') {
+            throw new Error("Callback function missing");
+        }
+
+        setTimeout(() => {
+            if (!contact || typeof contact !== 'object') {
+                let err = {};
+                err.code = 1002;
+                err.msg = "Please pass object"
+                callback(err);
+                return;
+            }
+
+            const requireFields = ['name', 'email', 'phone', 'city'];
+            const missingFields = [];
+
+            requireFields.forEach((field) => {
+                if (!(field in contact)) {
+                    missingFields.push(field);
+                }
+            });
+
+            if (missingFields.length !== 0) {
+                let err = {};
+                err.code = 1003;
+                err.msg = "Please supply missing fields " + missingFields.join();
+                callback(err);
+                return;
+            }
+
+            contact.id = ++this.data.idCounter;
+            this.data.contacts.push(contact);
+
+            fs.writeFile(fileName, JSON.stringify(this.data), 'utf-8', (err, data) => {
+                if (err) {
+                    callback(err);
+                    return;
+                }
+                callback(null, contact);
+            });
+
+        }, 0);
 
     }
+
+
 
 
 }
