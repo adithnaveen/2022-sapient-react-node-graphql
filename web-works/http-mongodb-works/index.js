5ad86ae4f88480842e7a3666ed7fbe6add0dc117
Naveen <adith.naveen@gmail.com>
2022-06-22 10:54:43 +0530

    updated mongodb code

--- web-works/http-mongodb-works/index.js
+++ web-works/http-mongodb-works/index.js
@@ -1,3 +1,21 @@
-import express from 'express'; 
-import mongoose from 'mongoose'
+import express from 'express';
+import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+
+
+// variable declaration
+const app = express();
+const PORT = 3000;
+
+// configuration
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ type: 'application/json' }));
+
+
+app.get("/", (req, res) => {
+    res.json({ "message": "Welcome" });
+})
+
+app.listen(PORT, () => {
+    console.log(`Server started at ${PORT}`);
+})
