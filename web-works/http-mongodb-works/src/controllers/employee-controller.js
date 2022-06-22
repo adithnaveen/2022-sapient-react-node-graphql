0730d224d08f599dc7caa1a505dc9d7afc82c3b1
Naveen <adith.naveen@gmail.com>
2022-06-22 15:10:35 +0530

    updated code auth

--- web-works/http-mongodb-works/src/controllers/employee-controller.js
+++ web-works/http-mongodb-works/src/controllers/employee-controller.js
@@ -110,16 +110,18 @@ export const deleteEmployeeById = (req, res) => {
 
 
 // authenticat user - (email, password)
-export const authenticateUser = async (req, rest) => {
+export const authenticateUser = async (req, res) => {
     try {
         let retEmployee = await Employee.findOne({ email: req.body.email });
         const validateEmployee = await compare(req.body.password, retEmployee.password);
 
         if (validateEmployee) {
             res.send({ message: "User Validated" });
+        } else {
+            res.send({ message: "Sorry not validate credentials :" + req.body.email });
         }
     } catch (err) {
-        res.send({ message: "Sorry not validate credentials :" + req.body.email });
+        res.send({ message: "Error Authenticating :" + req.body.email });
     }
 }
 
