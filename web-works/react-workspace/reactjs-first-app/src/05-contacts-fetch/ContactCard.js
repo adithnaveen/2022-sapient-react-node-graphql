731f45af673bd9c8609c556597b697f70b008f09
Naveen <adith.naveen@gmail.com>
2022-06-29 13:00:30 +0530

    completed contact card

---
+++ web-works/react-workspace/reactjs-first-app/src/05-contacts-fetch/ContactCard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const ContactCard = ({ contact }) => (
+
+    <div className="card">
+        <div className="row">
+            <div className="col-md-4">
+                <img src={contact.pic} alt={contact.name} className="card-img"
+                    style={{ width: '100px', height: '100px' }} />
+            </div>
+            <div className="col-md-8">
+                <div className="card-body">
+                    <h4 className="card-title">{contact.name}</h4>
+                    <div className="card-text">{contact.phone}</div>
+                    <div className="card-text">{contact.email}</div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+);
+
+export default ContactCard; 
--- web-works/react-workspace/reactjs-first-app/src/05-contacts-fetch/index.js
+++ web-works/react-workspace/reactjs-first-app/src/05-contacts-fetch/index.js
@@ -1,13 +1,29 @@
-import React, { Component } from 'react'; 
+import React, { Component } from 'react';
 
 
 class App extends Component {
-    state = { 
+    state = {
         contacts: []
-     }
+    }
+
+    // good place to get data 
+    componentDidMount() {
+        fetch("http://localhost:4000/contacts")
+            .then(resp => resp.json())
+            .then(data => this.setState({ contacts: data }));
+    }
+
     render() {
+
+        const contactsList = this.state.contacts.map(c => <ContactCard contact={c} />)
         return (
-            
+            <div className="container">
+                <h1 className='alert alert-info'>Contacts list</h1>
+                <h4>Number of contacts {this.state.contacts.length}</h4>
+
+                {contactsList}
+            </div>
+
         );
     }
 }
