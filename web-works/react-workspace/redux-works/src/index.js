d0025c370c3a5cf2b40074ed40a5118a0d1b0228
Naveen <adith.naveen@gmail.com>
2022-07-01 12:07:20 +0530

    updated with redux

--- web-works/react-workspace/redux-works/src/index.js
+++ web-works/react-workspace/redux-works/src/index.js
@@ -23,4 +23,40 @@ const reducer = (state = initialState, action) => {
 // as of now store is local variable we need to put it in window scope 
 const store = createStore(reducer);
 
-window['store'] = store; 
+window['store'] = store;
+
+const submitHandler = (evt) => {
+  evt.preventDefault();
+  const name = document.getElementById("name").value;
+
+  const action = { type: 'ADD_NAME', data: name };
+  store.dispatch(action);
+  document.getElementById('name').value = '';
+  document.getElementById('name').focus();
+}
+
+document.getElementById("frmName").onsubmit = submitHandler;
+
+
+const updateList = () => {
+  let names = store.getState();
+
+  const list = names.map((name, index) => '<li class="list-group-item"><button class="btn btn-danger" onclick="deleteName(' + index + ')">X</button>' + name + '</li>');
+
+  const listItems = list.join("");
+  document.getElementById("namesList").innerHTML = listItems;
+
+}
+
+window['updateList'] = updateList;
+updateList();
+
+store.subscribe(updateList);
+
+
+// delete part 
+window['deleteName'] = (index) => {
+  const action = { type: 'DELETE_NAME', data: index };
+  store.dispatch(action);
+}
+
