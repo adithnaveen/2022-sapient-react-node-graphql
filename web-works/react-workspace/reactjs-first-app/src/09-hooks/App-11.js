fa99985370074dbde37459c37b3cc8e8eef2047f
Naveen <adith.naveen@gmail.com>
2022-07-04 18:36:51 +0530

    updated with hooks fetch

--- web-works/react-workspace/reactjs-first-app/src/09-hooks/App-11.js
+++ web-works/react-workspace/reactjs-first-app/src/09-hooks/App-11.js
@@ -12,7 +12,11 @@ function App({ login }) {
     if (error) return <pre>{JSON.stringify(error, null, 2)} </pre>
     return (
         <div>
-            <pre>{JSON.stringify(data, null, 2)} </pre>
+            {/* <pre>{JSON.stringify(data, null, 2)} </pre> */}
+            <img src={data.avatar_url} alt={data.login} style={{ height: 200 }} />
+            <p>{data.login}</p>
+            {data.name && <p>{data.name}</p>}
+            {data.location && <p>{data.location}</p>}
         </div>
     )
 
