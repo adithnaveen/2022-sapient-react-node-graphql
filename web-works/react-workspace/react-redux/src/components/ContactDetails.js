3e9bf2c5ddf515c5bb995d6ee5cae798810da4ff
Naveen <adith.naveen@gmail.com>
2022-07-04 11:55:43 +0530

    updated work with contact details

--- web-works/react-workspace/react-redux/src/components/ContactDetails.js
+++ web-works/react-workspace/react-redux/src/components/ContactDetails.js
@@ -4,23 +4,46 @@ import { getContactById } from '../actions/contacts-action';
 
 class ContactDetails extends Component {
 
+
+    componentDidMount() {
+        let { id } = this.props.match.params;
+        this.props.getContact(id);
+    }
+
     render() {
 
-        let { id } = this.props.match.params;
 
 
 
+        let { contact } = this.props;
+        if (!contact) contact = {};
         return (
             <div className="container">
-                <h3>Contact Details of {id}</h3>
+                <h3>Contact Details </h3>
                 <div className="row">
                     <div className="col-md-4">
-                        Image
+                        <img src={contact.pic} alt={contact.name} style={{ height: 200 }} className="img img-thumbnail" />
+
                     </div>
                     <div className="col-md-8">
+                        <table className="table table-striped">
+                            <tbody>
+                                <tr>
+                                    <td className='table-primary'>Name</td>
+                                    <td className='card-title'>{contact.name}</td>
+                                </tr>
 
-                        Details
+                                <tr>
+                                    <td className='table-primary'>Email</td>
+                                    <td className='card-text'>{contact.email}</td>
+                                </tr>
 
+                                <tr>
+                                    <td className='table-primary'>Phone</td>
+                                    <td className='card-text'>{contact.phone}</td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
